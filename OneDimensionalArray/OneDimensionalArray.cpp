c2bc78929ae0d43398d984095edc4a3d838e83f3
Igor <zakatov0303@gmail.com>
2022-02-25 11:21:39 +0300

    OneDimensionalArray Added header files

--- OneDimensionalArray/OneDimensionalArray.cpp
+++ OneDimensionalArray/OneDimensionalArray.cpp
@@ -1,6 +1,19 @@
 ﻿#include <iostream>
+#include "Vector.h"
+#include "StaticArray.h"
+#include "DynamicArray.h"
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    
+    std::cout << "======Static array\n";
+    staticArray();
+
+    std::cout << "\n======Dynamic array\n";
+    dynamicArray();
+
+    std::cout << "\n======Vector\n";
+    vector();
+
+    return 0;
 }
